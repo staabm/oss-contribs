8cce2fe50a1474b1004712adea803bacb502f5e5
Markus Staab <maggus.staab@googlemail.com>
2026-03-01 20:36:48 +0100

    match pull request keywords in a case insensitive way

--- src/PullRequest/KeywordsParser.php
+++ src/PullRequest/KeywordsParser.php
@@ -16,35 +16,35 @@ final class KeywordsParser
 
         // find "closes phpstan/phpstan#10169"
         $matches = [];
-        preg_match_all('{('. implode('|', $keywords) .')\s+([a-z]+/[a-z]+#[0-9]+)}', $bodyText, $matches);
+        preg_match_all('{('. implode('|', $keywords) .')\s+([a-z]+/[a-z]+#[0-9]+)}i', $bodyText, $matches);
 
         $issues = [];
         foreach($matches[0] as $i => $match) {
             $issues[] = new IssueReference(
-                DescriptionKeyword::from($matches[1][$i]),
+                DescriptionKeyword::from(strtolower($matches[1][$i])),
                 $matches[2][$i]
             );
         }
 
         // find urls "closes https://github.com/phpstan/phpstan/issues/8366"
         $matches = [];
-        preg_match_all('{('. implode('|', $keywords) .')\s+https://github.com/([a-z]+/[a-z]+)/issues/([0-9]+)}', $bodyText, $matches);
+        preg_match_all('{('. implode('|', $keywords) .')\s+https://github.com/([a-z]+/[a-z]+)/issues/([0-9]+)}i', $bodyText, $matches);
 
         foreach($matches[0] as $i => $match) {
             $issues[] = new IssueReference(
-                DescriptionKeyword::from($matches[1][$i]),
+                DescriptionKeyword::from(strtolower($matches[1][$i])),
                 $matches[2][$i].'#'.$matches[3][$i]
             );
         }
 
         // find relative references "closes #597"
         $matches = [];
-        preg_match_all('{('. implode('|', $keywords) .')\s+(#[0-9]+)}', $bodyText, $matches);
+        preg_match_all('{('. implode('|', $keywords) .')\s+(#[0-9]+)}i', $bodyText, $matches);
 
         $urlParser = new PullRequestUrlParser($prUrl);
         foreach($matches[0] as $i => $match) {
             $issues[] = new IssueReference(
-                DescriptionKeyword::from($matches[1][$i]),
+                DescriptionKeyword::from(strtolower($matches[1][$i])),
                 $urlParser->getRepoIdentifier().$matches[2][$i]
             );
         }
